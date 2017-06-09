09f556f4c8fceb7c78ef55caf037c967f9969095
Srun <zhoucan007@me.com>
2017-06-09 12:14:16 +0800

    Initial Commit

---
+++ QRCodeScanner/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  QRCodeScanner
+//
+//  Created by Srun Zhou on 2017/6/9.
+//  Copyright © 2017年 Srun Zhou. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ QRCodeScanner/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  QRCodeScanner
+//
+//  Created by Srun Zhou on 2017/6/9.
+//  Copyright © 2017年 Srun Zhou. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
